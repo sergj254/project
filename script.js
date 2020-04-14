8c095f12969978d90ebbaf523a934e5691f8eab6
sergo <sergj254@mail.com>
2020-04-14 03:08:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let incr = 10;
+    docr = 10;
+    inch = true
+    incl = false
+// incr++;
+// docr--;
+console.log(incr--);
+console.log(docr++);
+console.log(5%2);
+console.log('2'==2);
+console.log(inch || incl);
